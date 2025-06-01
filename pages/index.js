4eb454e4cabd48874bb8c25fcdd6b0c336541e9f
jeffjeffjeff001 <jeff.larkin2014@gmail.com>
2025-06-01 16:33:19 -0400

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to Fantasy Spreads League</h1>
+      <p>This is the homepage. Visit <a href="/admin">/admin</a> to manage games.</p>
+    </div>
+  );
+}
