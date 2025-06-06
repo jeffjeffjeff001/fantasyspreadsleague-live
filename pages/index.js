003c0e0702ac83b68948e5d198a09d9ae9e9aeea
jeffjeffjeff001 <jeff.larkin2014@gmail.com>
2025-06-05 22:39:05 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to Fantasy Spreads League</h1>
-      <p>
-        This is the homepage. You can:
-      </p>
-      <ul>
-        <li>
-          <a href="/admin">Go to Admin (enter weekly games)</a>
-        </li>
-        <li>
-          <a href="/picks">Submit Your Picks</a>
-        </li>
-      </ul>
-    </div>
-  )
-}
+// pages/index.js
+ export default function Home() {
+   return (
+     <div style={{ padding: 20 }}>
+       <h1>Welcome to Fantasy Spreads League</h1>
+-      <p>
+-        This is the homepage. You can:
+-      </p>
+-      <ul>
+-        <li>
+-          <a href="/admin">Go to Admin (enter weekly games)</a>
+-        </li>
+-        <li>
+-          <a href="/picks">Submit Your Picks</a>
+-        </li>
+-      </ul>
++      <p>This is the homepage. You can:</p>
++      <ul>
++        <li>
++          <a href="/admin">Go to Admin (enter weekly games)</a>
++        </li>
++        <li>
++          <a href="/picks">Submit Your Picks</a>
++        </li>
++        <li>
++          <a href="/profile">View My Profile & Picks</a>
++        </li>
++      </ul>
+     </div>
+   )
+ }
