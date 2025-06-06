a45e710e7b395209e7614e4e9a3867fc491e8019
jeffjeffjeff001 <jeff.larkin2014@gmail.com>
2025-06-05 22:46:53 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,28 @@
 // pages/index.js
- export default function Home() {
-   return (
-     <div style={{ padding: 20 }}>
-       <h1>Welcome to Fantasy Spreads League</h1>
--      <p>
--        This is the homepage. You can:
--      </p>
--      <ul>
--        <li>
--          <a href="/admin">Go to Admin (enter weekly games)</a>
--        </li>
--        <li>
--          <a href="/picks">Submit Your Picks</a>
--        </li>
--      </ul>
-+      <p>This is the homepage. You can:</p>
-+      <ul>
-+        <li>
-+          <a href="/admin">Go to Admin (enter weekly games)</a>
-+        </li>
-+        <li>
-+          <a href="/picks">Submit Your Picks</a>
-+        </li>
-+        <li>
-+          <a href="/profile">View My Profile & Picks</a>
-+        </li>
-+      </ul>
-     </div>
-   )
- }
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to Fantasy Spreads League</h1>
+      <p>This is the homepage. You can:</p>
+      <ul>
+        <li>
+          <Link href="/admin">
+            <a>Go to Admin (enter weekly games)</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/picks">
+            <a>Submit Your Picks</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/profile">
+            <a>View My Profile & Picks</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
