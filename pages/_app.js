baac9d117a0666e20070a2351b1710b6e641fd3e
jeffjeffjeff001 <jeff.larkin2014@gmail.com>
2025-06-22 19:35:09 -0400

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+// pages/_app.js
+import { AuthProvider } from '../context/AuthContext'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
